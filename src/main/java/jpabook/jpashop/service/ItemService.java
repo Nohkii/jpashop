162146da1f7d575681f2a592ffe92225b432192f
Nohkii <120151129+Nohkii@users.noreply.github.com>
2024-04-01 10:00:56 +0900

    feat: 상품 수정 기능 추가
    - updateItemForm 수정 화면 추가
    - ItemService 상품 수정 메서드 추가
    - ItemController 상품 수정 메서드 추가

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -20,11 +20,19 @@ public class ItemService {
         itemRepository.save(item);
     }
 
-    public List<Item> findItems(){
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+    }
+
+    public List<Item> findItems() {
         return itemRepository.findAll();
     }
 
-    public Item findOne(Long itemId){
+    public Item findOne(Long itemId) {
         return itemRepository.findOne(itemId);
     }
 }
